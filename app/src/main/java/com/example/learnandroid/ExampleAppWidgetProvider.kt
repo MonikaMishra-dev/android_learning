becc175304690b0f90d25c18e70cf47f81f2dbc4
Monika <monikamonu9694@gmail.com>
2022-09-01 20:42:13 +0530

    first commit

---
+++ app/src/main/java/com/example/learnandroid/ExampleAppWidgetProvider.kt
@@ -0,0 +1,42 @@
+package com.example.learnandroid
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+
+class ExampleAppWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // Perform this loop procedure for each widget that belongs to this
+        // provider.
+        appWidgetIds.forEach { appWidgetId ->
+            // Create an Intent to launch ExampleActivity.
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(
+                /* context = */ context,
+                /* requestCode = */  0,
+                /* intent = */ Intent(context, MainActivity::class.java),
+                /* flags = */ PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            // Get the layout for the widget and attach an on-click listener
+            // to the button.
+            val views: RemoteViews = RemoteViews(
+                context.packageName,
+                R.layout.example_simple_widget_
+            ).apply {
+                setOnClickPendingIntent(R.id.btn_widget, pendingIntent)
+            }
+
+            // Tell the AppWidgetManager to perform an update on the current
+            // widget.
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
